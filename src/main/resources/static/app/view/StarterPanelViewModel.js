f09f26e1b874256622f372f6fad73af9fe18c4c8
leon <lchit18@freeuni.edu.ge>
2020-11-02 14:52:25 +0400

    new project

--- src/main/resources/static/app/view/StarterPanelViewModel.js
+++ src/main/resources/static/app/view/StarterPanelViewModel.js
@@ -8,17 +8,14 @@ Ext.define('EL.view.StarterPanelViewModel', {
                 children:[{
                     text:'VBox',
                     className:"EL.view.VBox",
-                    iconCls: 'fa fa-vbox',
                     leaf:true
                 },{
                     text:'HBox',
                     className:"EL.view.HBox",
-                    iconCls: 'fa fa-hbox',
                     leaf:true
                 },{
                     text:'Fit',
                     className:"EL.view.Fit",
-                    iconCls: 'fa fa-fit',
                     leaf:true
                 },
                 ]
